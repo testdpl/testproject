431ed1cdd5823042cdd249c7320f78195503c466
ubaid <ubaid.q@dplit.com>
2019-04-11 11:29:05 +0500

    changing branch

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 // Silence is golden.
 date_isodate_setdasdas
 asda
+<<<<<<< HEAD
 dasdb
 adnjasd
 a;sodium_crypto_auth_keygenaskldakl
 flsndlkf
 asfn
 ldsf
+=======
+check this file as well
+>>>>>>> master
