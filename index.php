a86ebe2a74a3b35d86ead2c17b980b1ab6989c3e
ubaid <ubaid.q@dplit.com>
2019-04-11 11:29:42 +0500

    changing branch

--- index.php
+++ index.php
@@ -2,13 +2,4 @@
 // Silence is golden.
 date_isodate_setdasdas
 asda
-<<<<<<< HEAD
-dasdb
-adnjasd
-a;sodium_crypto_auth_keygenaskldakl
-flsndlkf
-asfn
-ldsf
-=======
 check this file as well
->>>>>>> master
